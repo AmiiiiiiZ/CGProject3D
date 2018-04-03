35230ee6ece8023e4aa639518e35753075aba6c5
AmiiiiiiZ <AmiZhou@users.github.com>
2018-04-03 23:10:51 +1000

    Updated my test project - Amy
    Added some open source materials in folders.
    My project code became more complicated.

--- ProjectFiles/scripts/CreateObjects.js
+++ ProjectFiles/scripts/CreateObjects.js
@@ -4,18 +4,39 @@ function getBox(w, h, d) {
     var geometry = new THREE.BoxGeometry(w, h, d);
 
     // Phong material can interact with light.
-    var material = new THREE.MeshPhongMaterial({
-        color: 0x0000ff
+    var material = new THREE.MeshStandardMaterial({
+        color: 0xffffff
         });
 	var mesh = new THREE.Mesh(
 		geometry,
 		material 
     );
 
+    // Manipulate materials with texture
+    material.roughnessMap = loader.load('img/textures/scratch.jpg');
+    material.metalness = 0.5;
+    material.roughness = 0.7;
+    material.envMap = CubeMap();
+
     // Make the box cast shadow.
     mesh.castShadow = true;
 
 	return mesh;
+}
+
+// Cube map for 6 faces of a cube
+function CubeMap() {
+    var path = 'img/cubemap/';
+    var format = '.jpg';
+    var urls = [
+        path + 'px' + format, path + 'nx' + format,
+        path + 'py' + format, path + 'ny' + format,
+        path + 'pz' + format, path + 'nz' + format,
+    ]
+    var reflectionCube = new THREE.CubeTextureLoader().load(urls);
+    reflectionCube.format = THREE.RGBFormat;
+
+    return reflectionCube;
 }
 
 // Crate a group of boxes.
@@ -47,9 +68,14 @@ function getBoxGrid(amount, separationMultiplier) {
 
 // Create a plane square.
 function getPlane(size) {
-    var geometry = new THREE.PlaneGeometry(size, size);
-    var material = new THREE.MeshPhongMaterial({
-        color: 0xff0000,
+    // The last two params are segments(vertices).
+    var geometry = new THREE.PlaneGeometry(size, size,size*10,size*10);
+    geometry.vertices.forEach(function (vertex) {
+        vertex.z = Math.random() *0.2;
+    });
+
+    var material = new THREE.MeshStandardMaterial({
+        color: 0xffeeee,
         side: THREE.DoubleSide
     });
     var mesh = new THREE.Mesh(
@@ -59,6 +85,33 @@ function getPlane(size) {
 
     // Make the plane show the shadow of other objects.
     mesh.receiveShadow = true;
+
+    // Manipulate materials with texture
+    material.map = loader.load('img/textures/concrete.jpg');
+    // Uneven surface.
+    material.bumpMap = loader.load('img/textures/concrete.jpg');
+    material.bumpScale = 0.02;
+    material.roughnessMap = loader.load('img/textures/concrete.jpg');
+    material.metalness = 0.1;
+    material.roughness = 0.7;
+
+    //Reflection of the environment.
+    material.envMap = CubeMap();
+
+
+    // Repeat texture to have higher resolution.
+    //var texture = material.map;
+    //texture.wrapS = THREE.RepeatWrapping;
+    //texture.wrapT = THREE.RepeatWrapping;
+    //texture.repeat.set(1.5, 1.5);
+    //
+    //var maps = ['map', 'bumpMap', 'roughnessMap'];
+    //maps.forEach(function (mapName) {
+    //    var texture = material[mapName];
+    //    texture.wrapS = THREE.RepeatWrapping;
+    //    texture.wrapT = THREE.RepeatWrapping;
+    //    texture.repeat.set(1.5, 1.5);
+    //});
 
     return mesh;
 }
@@ -82,10 +135,50 @@ function getSphere(size) {
 
 // Create a point light like a light bulb.
 function getPointLight(intensity) {
-    var light = new THREE.PointLight(0xffffff, intensity);
+    var light = new THREE.PointLight('rgb(255,220,180)', intensity);
 
     // Make the light have shadow effect.
     light.castShadow = true;
+
+    return light;
+}
+
+
+// Create a spot light like at stage.
+function getSpotLight(intensity) {
+    var light = new THREE.SpotLight('rgb(255,220,180)', intensity);
+
+    // Make the light have shadow effect.
+    light.castShadow = true;
+
+    light.shadow.bias = 0.001;  // Eliminate gliches of the shadow.
+    light.shadow.mapSize.width = 2048;  // Make the shadow clearer. Default is 1024.
+    light.shadow.mapSize.height = 2048;
+
+    return light;
+}
+
+// Create a directional light like a sun far away.
+function getDirectionalLight(intensity) {
+    var light = new THREE.DirectionalLight(0xffffff, intensity);
+
+    // Make the light have shadow effect.
+    light.castShadow = true;
+
+    // Make the shadow field larger.
+    light.shadow.camera.left = -5;
+    light.shadow.camera.bottom = -5;
+    light.shadow.camera.right = 5;
+    light.shadow.camera.top = 5;
+
+    return light;
+}
+
+// Create ambient light. Used to create uniform effect (reflection) on all objects.
+function getAmbientLight(intensity) {
+    var light = new THREE.AmbientLight('rgb(30,30,30)', intensity);
+
+    // Ambient light makes no shadow
 
     return light;
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CGProject3D
+
+# Come here to use the loaders to load 3D models to the scene.
+#https://github.com/mrdoob/three.js/tree/dev/examples/js/loaders
+
+# Available 3D model sites apart from turbosquid: sketchfab.com clara.io 
